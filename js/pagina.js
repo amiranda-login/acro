c56a13fa8539e84eb2f4b13fea3cab4d43985ccf
amiranda <amiranda@logintechcr.com>
2020-02-05 12:17:28 -0600

    sync

--- js/pagina.js
+++ js/pagina.js
@@ -2,4 +2,12 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.carousel').carousel();
+
+    $("[href^='#sec']").click(function(){
+        var id = $(this).attr('href').replace('#','');
+         $('html,body').animate({
+            scrollTop: $("#" + id).offset().top
+        }, 'slow');
+        return false;
+    })
   });
